42c5950cebd7ff89f087cd513a21be28b4b2ec13
cmkshaw <caroline.shaw@gmail.com>
2014-10-20 23:01:57 -0400

    Meta tags

--- header.php
+++ header.php
@@ -11,6 +11,11 @@
 <head>
     <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta property="og:title" content="Colin Smith Woodworking" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://colinsmithwoodworking.com" />
+    <meta property="og:image" content="http://colinsmithwoodworking.com/wp-content/uploads/2014/09/LOGLOGO1801.jpg" />
+    <meta name="description" content="Colin Smith is a cabinet maker and carpenter in Toronto, ON.">
     <title><?php wp_title( '' ); ?><?php if (wp_title( '', false )) { echo ' |'; } ?> <?php bloginfo( 'name' ); ?></title>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link href='http://fonts.googleapis.com/css?family=Lato|PT+Mono' rel='stylesheet' type='text/css'>
@@ -31,7 +36,7 @@
                     <h2><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php echo get_bloginfo( 'name' ); ?></a></h2>
                 <?php } ?>
             </div><!-- .logo -->
-            <h1 class="logoTitle">Colin Smith<br>Woodworking</h1>
+            <h1 class="logoTitle"><a href="/">Colin Smith<br>Woodworking</a></h1>
             <a id="menu-toggle" href="#">Menu</a>
 			<nav id="site-navigation" class="navigation main-navigation grid-1 clr" role="navigation">
 				<?php wp_nav_menu( array(
